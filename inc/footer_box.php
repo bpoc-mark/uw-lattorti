9dd8b58ca41a6e69e461106e7a2a4b23a92f5b7b
bpoc-mark <markariel.maata@gmail.com>
2021-09-02 14:26:15 +0800

    added footer pc and sp view

--- inc/footer_box.php
+++ inc/footer_box.php
@@ -1,18 +1,72 @@
 <div class="footer_inner">
 
-	<div class="logo"><a href="/"><img src="" alt="<?php echo SITE_NAME; ?>"></a></div>
+	<div class="banner_section">
+		<div class="container row">
+			<div class="col">
+				<p>ご予約は、お電話または<br>予約サイトから承っております。</p>
+				<p><span>Tel.</span>053-424-6757</p>
+				<a href="">RESERVE</a>
+			</div>
+			<div class="col">
+				<h5>RECRUIT</h5>
+				<p>スタッフ募集中<br>
+				<span>再現性の高い技術を修得しながら一緒に<br>楽しく働いてみませんか？</span>
+				</p>
+				<a href="">view more</a>
+			</div>
+		</div>
+	</div>
 
-	<nav>
+	<div class="logo container">
+		<a href="/"><img src="/images/common/footer_logo.svg" alt="<?php echo SITE_NAME; ?>"></a>
+	</div>
+
+	<nav class="container footer_nav">
 		<ul>
-			<li><a href="/">ホーム</a></li>
-			<li><a href="/about/">このサイトついて</a></li>
-			<li><a href="/sitemap">サイトマップ</a></li>
-			<li><a href="/privacy">プライバシーポリシー</a></li>
+			<li><a href="/">SALON</a></li>
+			<li><a href="/">GALLERY</a></li>
+			<li><a href="/">HAIR</a></li>
+			<li><a href="/">NAIL</a></li>
+			<li><a href="/">EYELASH</a></li>
+			<li><a href="/">ESTHETIC</a></li>
+			<li><a href="/">STAFF</a></li>
+			<li><a href="/">ACCESS</a></li>
+			<li><a href="/">TOPICS</a></li>
+			<li><a href="/">RECRUIT</a></li>
+			<li><a href="/">CONTACT</a></li>
+			<li><a href="/">RESERVE</a></li>
 		</ul>
 	</nav>
 
-	<div class="copy">
-		<small>&copy; ウルトラワークス　All Lights Reserved.</small>
+	<div class="footer_deatils container">
+		<div class="row">
+			<div class="col">
+				<h3>l.a.ttorti total beauticians <br>
+					<span>（ラットルティ トータルビューティシャンズ 浜松）</span>
+				</h3>
+				<p>〒433-8122 静岡県浜松市中区上島3-17-3　<br>Tel. 053-424-6757</p>
+				<p>Open. ［火・水］10:00～18:00　<br> ［木・金］10:00～20:00 <br> 
+					［土］9:00～20:00　<br> ［日祝］9:00～17:00</p>
+				<p>Close. 月曜日</p>
+			</div>
+			<div class="col">
+				<h3>l.a.ttorti total beauticians sapporo<br>
+					<span>（ラットルティ トータルビューティシャンズ 札幌）</span>
+				</h3>
+				<p>〒062-0933 札幌市豊平区平岸三条15丁目3-3　<br>Tel. 011-598-0850 <br>
+					Open. 11:00～16:00　<br>Close／不定休</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="copy container">
+		<a href="">PRIVACY POLICY</a>
+		<small>&copy; l.a.ttorti All Rights Reserved.</small>
+	</div>
+
+	<div class="footer_btn">
+		<a href="">RESERVE</a>
+		<a href="tel:053-424-6757"><span>Tel.</span>053-424-6757</a>
 	</div>
 
 </div>
