f55e8bd40ef14f7b0bcdd185232184d0ac72b10b
bpoc-mark <markariel.maata@gmail.com>
2021-09-02 16:15:54 +0800

    added section 5 pc and sp view

---
+++ inc/top/section_5.php
@@ -0,0 +1,138 @@
+<section class="sect_5">
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p>ヘアスタイル</p>
+                    <h3>HAIR STYLE</h3>
+                    <a href="">view more</a>
+                </div>
+                <div class="col">
+                    <div class="img_cont">
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/1.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/1.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/1.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/2.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/2.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/2.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/3.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/3.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/3.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/4.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/4.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/4.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/5.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/5.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/5.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/6.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/6.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/6.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                    </div>
+                    <a href="">view more</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p>ネイルデザイン</p>
+                    <h3>NAIL DESIGN</h3>
+                    <a href="">view more</a>
+                </div>
+                <div class="col">
+                    <div class="img_cont">
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/n_1.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/n_1.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/n_1.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/n_2.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/n_2.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/n_2.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/n_3.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/n_3.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/n_3.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/n_4.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/n_4.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/n_4.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/n_5.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/n_5.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/n_5.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                        <div class="img_col">
+                            <picture>
+                            <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                                <source srcset="/images/top/sp/section_5/n_6.png" media="(max-width: 899px)" type="image/webp">
+                                <source srcset="/images/top/section_5/n_6.png" media="(min-width: 900px)">
+                                <img src="/images/top/sp/section_5/n_6.png" alt="">
+                            </picture>
+                            <p class="desc">ワンレン大人カジュアルボブパーマワンレン大人カジュアル</p>
+                        </div>
+                    </div>
+                    <a href="">view more</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
