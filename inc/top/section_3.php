7826fbbcb657491bccfa77b60689659e2b65f718
bpoc-mark <markariel.maata@gmail.com>
2021-09-03 15:28:23 +0800

    added section 3 pc and sp view

---
+++ inc/top/section_3.php
@@ -0,0 +1,35 @@
+<section class="sect_3">
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <span class="txt_about">ABOUT</span>
+                    <h3>長くお付き合いいただける<br>
+                        あなただけの上質空間。</h3>
+                    <p>l.a.ttorti（ラットルティ）にはオートクチュールの洋服のように、お客様一人ひとりの美をトータルにサポートする、さまざまなこだわりがあります。<br>
+                        選べるシャンプーや炭酸泉、高い技術とスタイル提案、心ほどける空間づくり…。<br>
+                        一生お付き合いいただける特別なサロンでありたいと思っております。</p>
+                    <ul>
+                        <li>
+                            <a href=""><p><span>SALON</span><br>サロン情報</p></a>
+                        </li>
+                        <li>
+                            <a href=""><p><span>ACCESS</span><br>アクセス</p></a>
+                        </li>
+                        <li>
+                            <a href=""><p><span>STAFF</span><br>スタッフ紹介</p></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <picture>
+                    <!-- <source　srcset="/images/top/advan.webp" media="(min-width: 900px)" type="image/webp"> -->
+                        <source srcset="/images/top/sp/section_3/img_1.png" media="(max-width: 899px)" type="image/webp">
+                        <source srcset="/images/top/section_3/img_1.png" media="(min-width: 900px)">
+                        <img src="/images/top/sp/section_3/img_1.png" alt="">
+                    </picture>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
