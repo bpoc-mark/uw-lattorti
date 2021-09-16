4c904f1e5a008e427a972318628b1a4240b6297b
bpoc-mark <markariel.maata@gmail.com>
2021-09-16 17:05:23 +0800

    added recruit pc and sp view

---
+++ recruit/index.php
@@ -0,0 +1,285 @@
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/config.php"); ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/tag_manager.php"); ?>
+  <meta charset="UTF-8" />
+  <meta name="robots" content="index,follow">
+  <link rel="canonical" href="<?php echo CANONICAL ?>">
+  <meta name="keywords" content="<?php echo KEY_WORD ?>">
+  <meta name="description" content="<?php echo DESCRIPTION ?>">
+  <meta property="og:title" content="<?php echo SITE_NAME ?>">
+  <meta property="og:type" content="article" /> <!-- 下層では「article」にしてください -->
+  <meta property="og:description" content="<?php echo DESCRIPTION ?>" />
+  <meta property="og:url" content="<?php echo CANONICAL ?>" />
+
+  <title><?php echo SITE_NAME ?></title>
+
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_include.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/ga.php"); ?>
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/header_tag.php"); ?>
+</head>
+
+<body id="Recruit">
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/content_tag.php"); ?>
+  <div id="AllBox">
+    <header>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/header_box.php"); ?>
+    </header>
+
+    <main>
+      <section class="jumbo_sect">
+        <div class="wrapper">
+          <div class="container">
+            <span>採用情報</span>
+            <h3>RECRUIT</h3>
+            <ul class="breadcrumb">
+              <li>
+                  <a href="">TOP</a>
+              </li>
+              <li>
+                  <a href="">RECRUIT</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section class="sect_1">
+        <div class="wrapper">
+          <div class="txt_head container">
+            <h3 class="c-anim-up">すべてはお客様の笑顔のために。<br>
+                私たちの幸せは、<br>
+                お客様の笑顔によって創られます。</h3>
+            <p class="c-anim-up">をモットーに、スタイルの再現性を追求し、一生お付き合いさせてただける<br>特別なサロンでありたいと考えております。</p>
+          </div>
+          <div class="point_cont container">
+            <h3 class="c-anim-up">
+              <span class="c-anim-up">4つの安心ポイント</span><br>
+              4 POINT
+            </h3>
+            <div class="row">
+              <div class="col c-anim-up">
+                <span>1</span>
+                <h4>残業手当てが給付される自由な教育体制</h4>
+                <p>最低限の練習は営業時間内に行い、営業後の練習は自主(自由)練習となり残業手当が給付されます。個人の希望を尊重し、学びたい技術を優先して、ひとりひとり違うカリキュラムで教育を行っております。</p>
+              </div>
+              <div class="col c-anim-up">
+                <span>2</span>
+                <h4>完全週休2日制で年間休日104日以上</h4>
+                <p>1日8時間（休憩１時間）の営業、そして完全週休2日制で月に1度3連休があります。夏季・冬季休暇、有給休暇も含め年間休日は104日以上です。旅行やジム、飲み会などプライベートな時間が持てます。</p>
+              </div>
+              <div class="col c-anim-up">
+                <span>3</span>
+                <h4>スキルアップの為の資金無利息貸付制度あり</h4>
+                <p>スキルアップに必要な資金の無利子貸付制度があり、シザーなどの道具購入や資格取得の為に利用されています。専門学校授業料の融資制度は卒業後5年勤務で返済不要なので、通信制に通いながら勤務するスタッフも在籍しています。</p>
+              </div>
+              <div class="col c-anim-up">
+                <span>4</span>
+                <h4>専門講師によるハイレベルな講習</h4>
+                <p>美容商材製造メーカーや各種講師との提携で、外部講師の講習が店内で受けられます。スタッフの希望に応じた講師を招き、個人レッスンも実施しています。講師費用は会社負担なので費用面の心配なく学ぶことが出来ます。</p>
+              </div>
+            </div>
+          </div>
+          <div class="voice_cont container">
+            <h3 class=" c-anim-up">
+              <span>スタッフの声</span><br>
+              STAFF VOICE
+            </h3>
+            <div class="row">
+              <div class="col">
+                <img src="/images/recruit/1.png" alt="" class="c-anim-up">
+                <div class="txt_cont c-anim-up">
+                  <h4>好きな技術を学びながら、自分の 「美」も磨けます！</h4>
+                  <p>入社して16年目になりますが、日々新しいメニューや商品など、旬なアイテムを沢山取り入れ触れることが出来るので毎日が新鮮です。ヘア、アイラッシュ、ネイル、エステ、脱毛など自分が好きな技術を学びながら自分の美も磨いていけて嬉しいです。</p>
+                  <p><span>Stylist / Esthetician</span><br>太田 麻紀子</p>
+                </div>
+              </div>
+              <div class="col">
+                <img src="/images/recruit/2.png" alt="" class="c-anim-up">
+                <div class="txt_cont c-anim-up">
+                  <h4>営業後のトレージングには残業 手当てがつきます！</h4>
+                  <p>仕事とプライベートを両立しながら自分に合った働き方が出来ます。最先端の技術を日々学びながらプライベートな時間もしっかり持つ事が出来ます。<br>
+                      子育て中の私も学びながら自分のペースで働く事が出来るのでとてもありがたい環境です。</p>
+                  <p><span>Eyelist / Esthetician / Kitsuke</span><br> 小谷野 彩乃 （札幌店）</p>
+                </div>
+              </div>
+              <div class="col">
+                <img src="/images/recruit/3.png" alt="" class="c-anim-up">
+                <div class="txt_cont c-anim-up">
+                  <h4>学ぶ環境が整った働きやすい 美容室です！</h4>
+                  <p>営業中に練習が出来たり、営業後の自主練習には残業手当がついていたりと学ぶ環境が整っています。<br>
+                      また自分のペースで練習を進めることが出来るので、私には合っていて楽しいです。</p>
+                  <p><span>Assistant</span><br> 武田 純菜</p>
+                </div>
+              </div>
+              <div class="col">
+                <img src="/images/recruit/4.png" alt="" class="c-anim-up">
+                <div class="txt_cont c-anim-up">
+                  <h4>色々なジャンルの経験も積めて 楽しい職場です！</h4>
+                  <p>まつ毛エクステやまつ毛カール、ネイル、脱毛、エステなどヘア以外の技術が学べて、施術の経験が出来るので仕事が楽しいです。</p>
+                  <p><span>Stylist / Eyelist / Esthetician</span><br> 太田 可奈子</p>
+                </div>
+              </div>
+              <div class="col">
+                <img src="/images/recruit/5.png" alt="" class="c-anim-up">
+                <div class="txt_cont c-anim-up">
+                  <h4>営業後のトレージングには残業 手当てがつきます！</h4>
+                  <p>営業中にトレーニングができ、営業後のトレーニングも自由なのでたくさん学ぶ機会があります。営業後のトレーニングには残業手当もつくので、頑張る励みになります。</p>
+                  <p><span>Eyelist / Esthetician</span><br> 長谷川 紗菜</p>
+                </div>
+              </div>
+              <div class="col">
+                <img src="/images/recruit/6.png" alt="" class="c-anim-up">
+                <div class="txt_cont c-anim-up">
+                  <h4>月に1度の3連休でリフレッシュ できます！</h4>
+                  <p>月に１度、３連休があるので毎月旅行に行けちゃいます。<br>
+                      のんびりリフレッシュ出来るので仕事にも集中できます！</p>
+                  <p><span>Nailist / Eyelist / Esthetician</span><br> 荒木 萌華</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="img_cont container">
+            <div class="row">
+              <div class="col c-anim-up">
+                <img src="/images/recruit/img_4.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <img src="/images/recruit/img_3.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <img src="/images/recruit/img_2.png" alt="">
+              </div>
+              <div class="col c-anim-up">
+                <img src="/images/recruit/img_1.png" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="training_cont container">
+            <div class="row">
+              <div class="col c-anim-up">
+                <span class="col_head">TRAINING</span>
+                <div class="list">
+                  <span>教 育</span>
+                  <ul>
+                    <li>
+                      <p>・ 個人個人がどれだけお客様の気持ちを考えながら接客出来るかを考えています。</p>
+                    </li>
+                    <li>
+                      <p>・ “ルール”に縛られず自ら現場の空気を読み取り動けるようになり、自分の考え・責任で行動出来る人間に成長していけると考えます。</p>
+                    </li>
+                    <li>
+                      <p>・ ヘア、ネイル、マツエク、脱毛、エステの中で希望する技術を学べます。</p>
+                    </li>
+                    <li>
+                      <p>・ 掃除も挨拶もカットも練習も勉強も「すべてはお客様の笑顔のために」です。</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col c-anim-up">
+                <span class="col_head">CURRICULUM</span>
+                <div class="list">
+                  <span>カリキュラム</span>
+                  <ul>
+                    <li>
+                      <p>・ スタイリストデビューまで23項目(カット・ワインディング・カラー・アップ・メイク・学科)の試験があります。</p>
+                    </li>
+                    <li>
+                      <p>・ 昇給項目に合格すると翌月から昇給となります。</p>
+                    </li>
+                    <li>
+                      <p>・ スタイリストデビューまで3年を目標としています。</p>
+                    </li>
+                    <li>
+                      <p>・ 外部講師による講習会も多数開催。</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="information_cont container">
+            <div class="row">
+              <div class="col">
+                <h3 class="c-anim-up">
+                  <span>募集要項</span><br>
+                  INFORMATION
+                </h3>
+              </div>
+              <div class="col">
+                <div class="row c-anim-up">
+                  <div class="col">
+                    <p>募集職種</p>
+                  </div>
+                  <div class="col">
+                    <p>① 2022年3月に美容学校卒業予定の方<br>
+                        ② スタイリスト希望<br>
+                        ③ アシスタント希望<br>
+                        ④ アイリスト希望<br>
+                        ⑤ ネイリスト希望<br>
+                        ⑥ パート・アルバイト希望</p>
+                  </div>
+                </div>
+                <div class="row c-anim-up">
+                  <div class="col">
+                    <p>基本給</p>
+                  </div>
+                  <div class="col">
+                    <p>① 182,000円～ <br>
+                        ② 200,000円（スタイリストキャリア1年）＋歩合給～<br>
+                        ③ 182,000円～<br>
+                        ④ 182,000円＋歩合給～<br>
+                        ⑤ 182,000円＋歩合給～<br>
+                        ⑥ 950円/時給～</p>
+                  </div>
+                </div>
+                <div class="row c-anim-up">
+                  <div class="col">
+                    <p>休　日</p>
+                  </div>
+                  <div class="col">
+                    <p>・ 完全週休2日制（毎月1回3連休あり）<br>
+                      ・ 夏季・冬季休暇<br>
+                      ・ 有給制度</p>
+                  </div>
+                </div>
+                <div class="row c-anim-up">
+                  <div class="col">
+                    <p>福利厚生</p>
+                  </div>
+                  <div class="col">
+                    <p>・ 社会保険 <br>
+                      ・ 雇用保険<br>
+                      ・ 労災保険<br>
+                      ・ 交通費全額支給<br>
+                      ・ 産休育休制度あり<br>
+                      ・ OJT(営業時間内トレーニング)採用<br>
+                      ※ 営業後自主練習には残業手当がつきます</p>
+                  </div>
+                </div>
+                <div class="row c-anim-up">
+                  <div class="col">
+                    <p>お電話またはエントリーフォームより<br>ご応募ください。</p>
+                    <a href="tel:053-424-6757" class="btn_tel"><span>Tel.</span>053-424-6757</a>
+                  </div>
+                  <div class="col">
+                    <a href="" class="btn_entry">ENTRY FORM</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/footer_box.php"); ?>
+    </footer>
+  </div><!-- //AllBox -->
+  <?php include($_SERVER['DOCUMENT_ROOT'] . "/inc/tag/footer_tag.php"); ?>
+</body>
+
+</html>
